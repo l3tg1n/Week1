f8b00df41fdf9f06d7239c4011df311a9c6ab87c
l3tg1n <140387436+l3tg1n@users.noreply.github.com>
2023-07-25 00:26:41 +0300

    Create ex2.5.py

---
+++ ex2.5.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter first numver:"))
+num2 = int(input("Enter second numver:"))
+print("Result:", num1 + num2)
+print("Result:", num1 - num2)
+print("Result:", num1 * num2)
+print("Result:", num1 / num2)
